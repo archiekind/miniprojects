755207a3eccb114bac668cc662f6c0cf473a7ba8
archiekind <archiekind1@gmail.com>
2024-12-17 18:24:42 +0000

    added file storage of grids, bugfix required

--- Conway/conway.hs
+++ Conway/conway.hs
@@ -1,21 +1,28 @@
+import Control.Concurrent (threadDelay)
 
 
 type Cell = Bool
 type Grid = [[Cell]]
 
 dim :: Int
-dim = 3
+dim = 4
 
 initialGrid :: Grid
 initialGrid = [[False, True, False], [False, True, False], [False, True, False]]
 
+nextState :: Grid -> Int -> Int -> Grid -> Grid
+nextState [] _ dim _ = []
+nextState (row:rows) x y grid = g row grid : nextState rows x (y + 1) grid
 
--- nextState :: Grid -> Int -> Int -> Grid
--- nextState [] _ _ = []
--- nextState grid@(row:rows) x y = g row ++ nextState grid x (y + 1)
-
---   where g :: [Cell] -> grid -> [Cell]
---         g (e:ex) 
+  where g :: [Cell] -> Grid -> [Cell]
+        g [] _ = []
+        g (True:ex) grid
+          | countNeighbours x y grid < 2 = False : g ex grid
+          | countNeighbours x y grid > 3 = False : g ex grid
+          | otherwise = True : g ex grid
+        g (False:ex) grid
+          | countNeighbours x y grid == 3 =  True : g ex grid
+          | otherwise = False : g ex grid
   
   
 -- [[updateCell x y grid | y <- [0..dim]] | x <- [0..dim]]
@@ -30,15 +37,30 @@ countNeighbours x y (row:rows)
   where g :: Int -> Int -> [Cell] -> Int
         g x y [] = 0
         g x y (e:es)
-          | y == 0 && x == 0 = 0
+          | y == 0 && x == 0 = 0 + g (x - 1) y es
           | x < 2 && x > -2 && e = 1 + g (x - 1) y es
           | otherwise = 0 + g (x - 1) y es
 
+renderGrid :: Grid -> IO ()
+renderGrid = mapM_ (putStrLn . map (\c -> if c then '*' else '.'))
 
-  -- where
-  --   updateCell x y = applyRules (grid !! x !! y) (countLiveNeighbors x y)
-  --   countLiveNeighbors x y = sum [grid !! (x + dx) !! (y + dy) | dx <- [-1..1], dy <- [-1..1], (dx, dy) /= (0, 0)]
+runGame :: Grid -> IO ()
+runGame grid = do
+  renderGrid grid
+  threadDelay 500000           -- Pause for half a second
+  putStrLn "\ESC[2J"           -- Clear the screen
+  runGame (nextState grid 0 0 grid)
 
-applyRules :: Cell -> Int -> Cell
-applyRules True n  = n == 2 || n == 3  -- Alive cell stays alive if 2 or 3 neighbors
-applyRules False n = n == 3           -- Dead cell becomes alive if exactly 3 neighbors
+makeGrid :: [String] -> Grid
+makeGrid [] = []
+makeGrid (xs:xss) = g xs : makeGrid xss
+  where g :: [Char] -> [Cell]
+        g [] = []
+        g (x:xs) 
+          | x == '1' = True : g xs
+          | otherwise = False : g xs
+
+initialise :: IO ()
+initialise = do
+  gridStr <- readFile "grid.txt"
+  runGame (makeGrid (lines gridStr))
