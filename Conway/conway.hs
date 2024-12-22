c31626e3f72da691c694c2ff25da9b6d291c8954
archiekind <archiekind1@gmail.com>
2024-12-22 23:16:05 +0000

    added graphical rendering

--- Conway/conway.hs
+++ Conway/conway.hs
@@ -1,14 +1,11 @@
-import Control.Concurrent (threadDelay)
-
+import Graphics.Gloss
 
 type Cell = Bool
 type Grid = [[Cell]]
 
-dim :: Int
-dim = 4
-
-initialGrid :: Grid
-initialGrid = [[False, True, False], [False, True, False], [False, True, False]]
+xdim, ydim :: Int
+xdim = 11
+ydim = 18
 
 nextState :: Grid -> Grid
 nextState grid = [[ evolveCell x y grid | x <- [0..width-1]] | y <- [0..height-1]]
@@ -27,32 +24,50 @@ countNeighbours :: Int -> Int -> Grid -> Int
 countNeighbours x y grid = sum [fromEnum $ safeGet grid (x+dx) (y+dy) | dx <- [-1..1], dy <- [-1..1], not (dx == 0 && dy == 0)]
   where
     safeGet :: Grid -> Int -> Int -> Bool
-    safeGet g x y 
-      | x < 0 || y < 0 = False
-      | y >= length g = False
-      | x >= length (head g) = False
-      | otherwise = g !! y !! x
+    safeGet g a b 
+      | a < 0 || b < 0 = False
+      | b >= length g = False
+      | a >= length (head g) = False
+      | otherwise = g !! b !! a
 
-renderGrid :: Grid -> IO ()
-renderGrid = mapM_ (putStrLn . map (\c -> if c then '*' else '.'))
+window :: Display
+window = InWindow "Game of Life" (pixelSize * xdim, pixelSize * ydim) (200, 200)
 
-runGame :: Grid -> IO ()
-runGame grid = do
-  renderGrid grid
-  threadDelay 500000           -- Pause for half a second
-  putStrLn "\ESC[2J"           -- Clear the screen
-  runGame (nextState grid)
+background :: Color
+background = black
+
+rectangle :: Picture
+rectangle = color white (rectangleSolid (fromIntegral pixelSize) (fromIntegral pixelSize))
+
+drawing :: Grid -> [Picture]
+drawing [] = []
+drawing (row:rows) = g row (ydim `div` 2 - length rows) ++ drawing rows
+  where g :: [Cell] -> Int -> [Picture]
+        g [] _ = []
+        g (True:xs) d = translate (fromIntegral ((xdim `div` 2 - length xs) * pixelSize)) (fromIntegral(d * pixelSize)) rectangle : g xs d
+        g (False:xs) d = g xs d
+
+-- renderGrid :: Grid -> IO ()
+-- renderGrid grid = do 
+--   animate window background (\_ -> pictures (drawing grid))
 
 makeGrid :: [String] -> Grid
 makeGrid [] = []
 makeGrid (xs:xss) = g xs : makeGrid xss
   where g :: [Char] -> [Cell]
         g [] = []
-        g (x:xs) 
-          | x == '1' = True : g xs
-          | otherwise = False : g xs
+        g (x:xs') 
+          | x == '1' = True : g xs'
+          | otherwise = False : g xs'
 
-initialise :: IO ()
-initialise = do
+runGame :: Grid -> IO ()
+runGame initialGrid = simulate window background 2 initialGrid (pictures . drawing) (\_ _ grid -> nextState grid)
+
+pixelSize :: Int
+pixelSize = 10
+
+main :: IO ()
+main = do
   gridStr <- readFile "grid.txt"
   runGame (makeGrid (lines gridStr))
+
