9ab9672dc1cee849a292cb98735a8e0c718204bf
archiekind <archiekind1@gmail.com>
2024-12-22 21:24:03 +0000

    bugfixed Conway's and added example oscillation

--- Conway/conway.hs
+++ Conway/conway.hs
@@ -10,36 +10,28 @@ dim = 4
 initialGrid :: Grid
 initialGrid = [[False, True, False], [False, True, False], [False, True, False]]
 
-nextState :: Grid -> Int -> Int -> Grid -> Grid
-nextState [] _ dim _ = []
-nextState (row:rows) x y grid = g row grid : nextState rows x (y + 1) grid
+nextState :: Grid -> Grid
+nextState grid = [[ evolveCell x y grid | x <- [0..width-1]] | y <- [0..height-1]]
+  where
+    height = length grid
+    width = length (head grid)
+    evolveCell x y g
+      | alive && (n < 2 || n > 3) = False
+      | not alive && n == 3 = True
+      | otherwise = alive
+      where
+        alive = g !! y !! x
+        n = countNeighbours x y g
 
-  where g :: [Cell] -> Grid -> [Cell]
-        g [] _ = []
-        g (True:ex) grid
-          | countNeighbours x y grid < 2 = False : g ex grid
-          | countNeighbours x y grid > 3 = False : g ex grid
-          | otherwise = True : g ex grid
-        g (False:ex) grid
-          | countNeighbours x y grid == 3 =  True : g ex grid
-          | otherwise = False : g ex grid
-  
-  
--- [[updateCell x y grid | y <- [0..dim]] | x <- [0..dim]]
-
--- can you implement this using foldr
 countNeighbours :: Int -> Int -> Grid -> Int
-countNeighbours x y [] = 0
-countNeighbours x y (row:rows) 
-  | y < 2 && y > -2 = g x y row + countNeighbours x (y - 1) rows            -- count matches if on a neighbouring square
-  | otherwise = countNeighbours x (y - 1) rows
-
-  where g :: Int -> Int -> [Cell] -> Int
-        g x y [] = 0
-        g x y (e:es)
-          | y == 0 && x == 0 = 0 + g (x - 1) y es
-          | x < 2 && x > -2 && e = 1 + g (x - 1) y es
-          | otherwise = 0 + g (x - 1) y es
+countNeighbours x y grid = sum [fromEnum $ safeGet grid (x+dx) (y+dy) | dx <- [-1..1], dy <- [-1..1], not (dx == 0 && dy == 0)]
+  where
+    safeGet :: Grid -> Int -> Int -> Bool
+    safeGet g x y 
+      | x < 0 || y < 0 = False
+      | y >= length g = False
+      | x >= length (head g) = False
+      | otherwise = g !! y !! x
 
 renderGrid :: Grid -> IO ()
 renderGrid = mapM_ (putStrLn . map (\c -> if c then '*' else '.'))
@@ -49,7 +41,7 @@ runGame grid = do
   renderGrid grid
   threadDelay 500000           -- Pause for half a second
   putStrLn "\ESC[2J"           -- Clear the screen
-  runGame (nextState grid 0 0 grid)
+  runGame (nextState grid)
 
 makeGrid :: [String] -> Grid
 makeGrid [] = []
