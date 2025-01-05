7746a612081eaf63167acf2020cec69a39ec0779
archiekind <archiekind1@gmail.com>
2025-01-05 19:21:08 +0000

    added ability to add books

--- Review/app/Main.hs
+++ Review/app/Main.hs
@@ -1,26 +1,46 @@
 {-# LANGUAGE OverloadedStrings #-}
 
-import Network.Wai (Application, responseLBS) -- responseFile
+import Network.Wai (Application, responseLBS, requestMethod, rawPathInfo) -- responseFile
 import Network.Wai.Handler.Warp (run)
-import Network.HTTP.Types (status200)
+import Network.HTTP.Types (status200, status302)
+import Network.Wai.Parse (parseRequestBody, lbsBackEnd)
 import Paths_Review (getDataFileName)
 
 import Database.SQLite.Simple
+    ( Only(fromOnly), open, execute_, execute, query_, close)
 import Text.Mustache
 import Data.Aeson (object, (.=))
-import Data.Text.Lazy.Encoding (encodeUtf8)
+import qualified Data.Text.Lazy.Encoding as LE
+import qualified Data.Text.Encoding as E
+import Data.Text (unpack)
 
 app :: Application
 -- app _ respond = respond $ responseLBS status200 [("Content-Type", "text/plain")] "Hello, World!"
-app _ respond = do
-    filename <- getDataFileName "static/home.mustache"
-    template <- compileMustacheFile filename
-    books <- getBooks
-    let bookObj = map (\title -> object ["book" .= title]) books
-    let templateData = object ["books" .= bookObj]  
-    let renderedHTML = renderMustache template templateData
-    respond $ responseLBS status200 [("Content-Type", "text/html")] (encodeUtf8 renderedHTML)
-    -- respond $ responseFile status200 [("Content-Type", "text/html")] filename Nothing
+app request respond = case (requestMethod request, rawPathInfo request) of
+    ("POST", _) -> do
+        (params, _) <- parseRequestBody lbsBackEnd request
+
+        let title = fmap E.decodeUtf8 (lookup "title" params)
+        let author = fmap E.decodeUtf8 (lookup "author" params)
+        case (title, author) of
+            (Just t, Just a) -> do 
+                addBook (unpack t) (unpack a)
+            (_, _) -> do 
+                pure ()
+        
+        respond $ responseLBS status302 [("Location", "http:")] ""
+
+    (_, _) -> do
+        filename <- getDataFileName "static/home.mustache"
+        template <- compileMustacheFile filename
+        books <- getBooks
+        let bookObj = map (\title -> object ["book" .= title]) books
+        let templateData = object ["books" .= bookObj]  
+        let renderedHTML = renderMustache template templateData
+        respond $ responseLBS status200 [("Content-Type", "text/html")] (LE.encodeUtf8 renderedHTML)
+
+
+--     -- respond $ responseFile status200 [("Content-Type", "text/html")] filename Nothing
 
 -- Main function to start the server
 main :: IO ()
@@ -32,7 +52,15 @@ getBooks :: IO [String]
 getBooks = do
     let dbName = "data/books.db"
     database <- open dbName
-    execute_ database "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, author NOT NULL);"
+    execute_ database "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, author TEXT NOT NULL);"
     strs <- query_ database "SELECT title FROM books" :: IO [Only String]
     close database
     return (map fromOnly strs)
+
+addBook :: String -> String -> IO ()
+addBook title author = do
+    let dbName = "data/books.db"
+    database <- open dbName
+    execute_ database "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, author TEXT NOT NULL);"
+    execute database "INSERT INTO books(title, author) values(?, ?);" (title, author)
+    close database
--- Review/test.hs
+++ Review/test.hs
@@ -2,11 +2,20 @@
 
 import Database.SQLite.Simple
 
-getBooks :: IO [String]
-getBooks = do
+-- getBooks :: IO [String]
+-- getBooks = do
+--     let dbName = "data/books.db"
+--     database <- open dbName
+--     execute_ database "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, author NOT NULL);"
+--     strs <- query_ database "SELECT title FROM books" :: IO [Only String]
+--     close database
+--     return (map fromOnly strs)
+
+addBook :: String -> String -> IO ()
+addBook title author = do
     let dbName = "data/books.db"
     database <- open dbName
-    execute_ database "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, author NOT NULL);"
-    strs <- query_ database "SELECT title FROM books" :: IO [Only String]
+    execute_ database "CREATE TABLE IF NOT EXISTS books (id INTEGER PRIMARY KEY AUTOINCREMENT, title TEXT NOT NULL, author TEXT NOT NULL);"
+    execute database "INSERT INTO books(title, author) values(?, ?);" (title, author)
     close database
-    return (map fromOnly strs)
+    putStrLn "data inserted successfully"
